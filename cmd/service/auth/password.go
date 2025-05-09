0c9f26be8d5cfaeeaf2a2112b55fa5b8aeccbf48
josseycodes1 <adewumijosephine1@gmail.com>
2025-05-09 22:25:04 +0100

    check if user exists, then create if user doesn't exist

---
+++ cmd/service/auth/password.go
@@ -0,0 +1,14 @@
+package auth
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	// Hash the password using the bcrypt library
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hash), nil
+}
--- cmd/service/user/routes.go
+++ cmd/service/user/routes.go
@@ -1,15 +1,17 @@
 package user
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/josseycodes1/ecom-api/cmd/service/auth"
 	"github.com/josseycodes1/ecom-api/types"
 	"github.com/josseycodes1/ecom-api/utils"
 )
 
 type Handler struct {
-	store *types.UserStore
+	store types.UserStore
 }
 
 func Newhandler(store types.UserStore) *Handler {
@@ -33,8 +35,30 @@ func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
 		utils.WriteError(w, http.StatusBadRequest, err)
 	}
 
-	//check if user exist by checking the database
+	// Check if user exists
+	_, err := h.store.GetUserByEmail(payload.Email)
+	if err == nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("user with email %s already exists", payload.Email))
+		return
+	}
+
+	// Hash password
+	hashedPassword, err := auth.HashPassword(payload.Password)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
 
 	//create user if it doesnt exist
 
+	err = h.store.CreateUser(types.User{
+		FirstName: payload.FirstName,
+		LastName:  payload.LastName,
+		Email:     payload.Email, //email is unique
+		Password:  hashedPassword,
+	})
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
 }
